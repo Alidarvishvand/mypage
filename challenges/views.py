a8f3cd63e0878cbc2a6037a3d47f8db1951c1d7a
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-10-08 14:11:48 +0330

    urls & views
    +returning  HTML
    +practicing urls , views & dinamic views

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,53 @@
 from django.shortcuts import render
-from  django.http import HttpResponse
+from  django.http import HttpResponse, HttpResponseNotFound , HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
+monthly_challenges = {
+    "january":"January hello",
+    "February":"February hello",
+    "march":"March hello",
+    "April":"April hello",
+    "May":"May hello",
+    "June":"June hello",
+    "July":"July hello",
+    "August":"August hello",
+    "September":"September hello",
+    "October":"October hello",
+    "November":"November hello",
+    "December":"December hello"
+}
+
 def index(request):
-    return HttpResponse('<h1>wwsss</h1>')
+    list_items = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
+
+
+
+def monthly_challenge_by_number(request, month): 
+    months = list(monthly_challenges.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+
+    
+    redirect_month = months[month - 1]
+    redirect_path = reverse("month-challenge", args=[redirect_month])
+
+    return HttpResponseRedirect("/challenges/" + redirect_month)
+
+def monthly_challenge(request, month):
+    try:
+        challenge_text = monthly_challenges[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound("this month is not supported")
+    
