7cec90e3e6465c4f0a932424fb1fb9607a9519ed
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-10-09 19:44:52 +0330

    filters
    +filters
    +extensions
    +tags
    +url tag for dinamic
    +if & else

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from  django.http import HttpResponse, HttpResponseNotFound , HttpResponseRedirect
 from django.urls import reverse
+
+
 # Create your views here.
 
 monthly_challenges = {
@@ -21,13 +23,15 @@ monthly_challenges = {
 def index(request):
     list_items = ""
     months = list(monthly_challenges.keys())
-
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html",{
+        "months": months
+    })
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
 
 
 
@@ -42,12 +46,15 @@ def monthly_challenge_by_number(request, month):
     redirect_month = months[month - 1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
 
-    return HttpResponseRedirect("/challenges/" + redirect_month)
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
     try:
-        challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        challenge_text = monthly_challenges[month] 
+        return render(request, "challenges/challenge.html",{
+            "text": challenge_text,
+            "month_name": month.capitalize()
+        })
     except:
         return HttpResponseNotFound("this month is not supported")
     
