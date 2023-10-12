97938590727a92e445f7ead3c62fa1729a29c247
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-10-12 11:29:43 +0330

    css & html
    created some css code added some code in settings
    +created some html code

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from  django.http import HttpResponse, HttpResponseNotFound , HttpResponseRedirect
 from django.urls import reverse
-
+from django.template.loader import render_to_string
 
 # Create your views here.
 
@@ -56,5 +56,6 @@ def monthly_challenge(request, month):
             "month_name": month.capitalize()
         })
     except:
-        return HttpResponseNotFound("this month is not supported")
+        response_data= render_to_string("404.html",)
+        return HttpResponseNotFound(response_data)
     
